cb90867f45b1818e6e300d468e9edac7f34296a7
ACHRAFHED <HEDDAD_MOHAMMEDACHRAF@emsi-edu.ma>
2023-12-29 16:37:07 +0000

    C2

--- src/main/java/com/example/demospringcloudstreamskafka/services/PageEventSevice.java
+++ src/main/java/com/example/demospringcloudstreamskafka/services/PageEventSevice.java
@@ -1,9 +1,18 @@
 package com.example.demospringcloudstreamskafka.services;
 
 import com.example.demospringcloudstreamskafka.entities.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.internals.TimeWindow;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -20,23 +29,37 @@ public class PageEventSevice {
             System.out.println("******************");
         };
     }
-    public Supplier<PageEvent> pageEventSupplier()
-    {
-        return () ->  new PageEvent(
-            Math.random()>0.5? "P1" :"P2",
-            Math.random()>0.5? "U1" :"U2",
-            new Date(),
-            new Random().nextInt(9000));
+@Bean
+    public Supplier<PageEvent> pageEventSupplier() {
+        return () -> new PageEvent(
+                Math.random() > 0.5 ? "P1" : "P2",
+                Math.random() > 0.5 ? "U1" : "U2",
+                new Date(),
+                new Random().nextInt(9000));
 
     }
+
     @Bean
-    public Function<PageEvent,PageEvent> pageEventFunction()
-    {
-        return (input) ->{
+    public Function<PageEvent, PageEvent> pageEventFunction() {
+        return (input) -> {
             input.setName(input.getName().toUpperCase());
-            input.setUser("UUUUU");
+            input.setUser(input.getUser().toUpperCase());
             return input;
         };
 
+    }
+
+    @Bean
+    public  Function<KStream<String,PageEvent>,KStream<String,Long>> KstreamFunction(){
+        return  (input)->{
+            return input
+                    .filter((k,v)->v.getDuration()>100)
+                    .map((k,v)->new KeyValue<>(v.getName(),0L))
+                    .groupBy((k,v)->k,Grouped.with(Serdes.String(),Serdes.Long()))
+                    .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                    .count(Materialized.as("page-count"))
+                    .toStream()
+                    .map((k,v)->new KeyValue<>("=>"+k.window().startTime()+k.window().endTime()+k.key(),v));
+        };
     }
 }
